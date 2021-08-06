7c1a046b7b4cefe47b6a6137cfb2fd67db7d529d
TrifoiIuliu <47026537+TrifoiIuliu@users.noreply.github.com>
2021-08-06 15:14:31 +0300

    Update ProgramExhaustor.ino

--- ProgramExhaustor/ProgramExhaustor.ino
+++ ProgramExhaustor/ProgramExhaustor.ino
@@ -2,7 +2,7 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
+// Setare LCD la adresa 0x27, 16 caractere pe linie si 2 linii
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 //Initializare variabile de program
@@ -16,14 +16,15 @@ ezButton stopBtn(4);
 
 void setup() {
   
-//initializare LCD
+//Initializare LCD
   lcd.backlight();
   lcd.begin();
   lastDisplay=0;
   currentDisplay=0;
-
+  
   digitalWrite(12,HIGH);
-//initializare intrari/iesiri
+  
+//Initializare intrari/iesiri
 pinMode(8, OUTPUT);
 pinMode(9, OUTPUT);
 pinMode(10, OUTPUT);
@@ -35,21 +36,16 @@ pinMode(3, INPUT);
 pinMode(4, INPUT);
 pinMode(5, INPUT);
 
-//setare butoane
+//Setare butoane
 setupBtn.setDebounceTime(100);
-setupBtn.setCountMode(COUNT_FALLING);
-
 startBtn.setDebounceTime(50);
-//startBtn.setCountMode(COUNT_FALLING);
-
 stopBtn.setDebounceTime(50);
-//stopBtn.setCountMode(COUNT_FALLING);
 
-//initializare butoane
+//Initializare comanda motor
  motorCmd=0;
   
-// initializare timere
-
+// Initializare timere
+  
 //Timer1 pentru inerval pauza
 timer1_preset=map(analogRead(A0), 0, 1024, 10000,300000);
 timer1_dn=0;
@@ -82,11 +78,9 @@ TIMSK1 |= (1<<OCIE1A);  //enable OCR1A
 
 ISR(TIMER1_COMPA_vect){
 
-  //Core of timer1
+  //Timer1
   if(timer1_acc<timer1_preset&&timer1_ON&&timer1_dn==0)
 {
-   //Serial.println("timer1: ");
-   //Serial.println(timer1_acc);
   timer1_acc+=1;
    
   
@@ -100,11 +94,9 @@ else
   if(!timer1_ON)
   timer1_acc=0;
 
-  //Core of timer2
+  //Timer2
  if(timer2_acc<timer2_preset&&timer2_ON&&timer2_dn==0)
 {  
-    //Serial.println("timer2: ");
-    //Serial.println(timer2_acc);
   timer2_acc+=1;
    
 } 
@@ -117,11 +109,9 @@ else
   if(!timer2_ON)
   timer2_acc=0;
 
-  //Core of timer3
+  //Timer3
  if(timer3_acc<timer3_preset&&timer3_ON&&timer3_dn==0)
 { 
-    //Serial.println("timer3: ");
-    //Serial.println(timer3_acc);
   timer3_acc+=1;
 } 
 else
@@ -176,7 +166,6 @@ switch(state) {
     
       case 1:
      // Pauza ciclu curatare filtre
-    // Serial.println("Pauza ciclu curatare filtre ");
      motorCmd=1;
      ev1Cmd=0;
      ev2Cmd=0;
@@ -196,7 +185,6 @@ switch(state) {
     
       case 2:
     // Impuls EV1
-   // Serial.println("Impuls EV1 ");
     ev1Cmd=1;
     timer2_ON=1;
     if(timer2_dn)
@@ -210,7 +198,6 @@ switch(state) {
     
       case 3:
     // Pauza EV1
-   // Serial.println("Pauza EV1 ");
     ev1Cmd=0;
     timer3_ON=1;
     if(timer3_dn)
@@ -224,7 +211,6 @@ switch(state) {
     
       case 4:
      // Impuls EV2
-    // Serial.println("Impuls EV2 ");
     ev2Cmd=1;
     timer2_ON=1;
     if(timer2_dn)
@@ -238,7 +224,6 @@ switch(state) {
     
       case 5:
     // Pauza EV2
-  //  Serial.println("Pauza EV2 ");
       ev2Cmd=0;
     timer3_ON=1;
 
@@ -253,7 +238,6 @@ switch(state) {
     
       case 6:
     // Impuls EV3
-   // Serial.println("Impuls EV3 ");
       ev3Cmd=1;
     timer2_ON=1;
     if(timer2_dn)
@@ -267,7 +251,6 @@ switch(state) {
     
       case 7:
     // Pauza EV3
-   // Serial.println("Pauza EV3 ");
       ev3Cmd=0;
     timer3_ON=1;
 
@@ -282,7 +265,6 @@ switch(state) {
     
       case 8:
     // Impuls EV4
-   // Serial.println("Impuls EV4 ");
       ev4Cmd=1;
     timer2_ON=1;
     if(timer2_dn)
@@ -296,7 +278,6 @@ switch(state) {
 
       case 9:
     // Pauza EV4, reinitializare ciclu curatare
-   // Serial.println("Reluare");
      ev4Cmd=1;
      if(startVar==0)
      state=1;
@@ -318,7 +299,7 @@ else
   timer1_preset=map(analogRead(A0), 0, 1024, 10000,300000);
   timer3_preset=map(analogRead(A1), 0, 1024, 3000,120000);
 
-//set display Setup mode
+//Afisare informatii din modul setare pe display
   setupModeDisplay();
 
  }
@@ -326,7 +307,7 @@ else
 //Actionare iesiri Arduino
 actionareIesiri();
 
- //Resetare counter buton= trecere in modul normal de lucru
+ //Trecere in modul normal de lucru
   if(setupCount>=2)
   setupBtn.resetCount();
 
@@ -373,7 +354,7 @@ currentDisplay=millis();
 
  if(currentDisplay-lastDisplay>100)
  {
-//  afisare timp pauza ciclu 
+//  Afisare timp pauza ciclu 
  lcd.setCursor(0,0);
  lcd.print("Urmeaza");
  lcd.setCursor(8,0);
@@ -442,18 +423,18 @@ currentDisplay=millis();
   void setupModeDisplay()
 {
 
-//  Afisare display la x ms informatiile ce urmeaza a fi setate
+//Afisare pe display la x ms informatiile ce urmeaza a fi setate
 currentDisplay=millis();
  if(currentDisplay-lastDisplay>100)
  {
-//  afisare timp pauza ciclu 
+//Afisare timp pauza ciclu 
  lcd.setCursor(0,0);
  lcd.print("Tpc:");
  lcd.setCursor(4,0);
  lcd.print((float)timer1_preset/1000);
  lcd.setCursor(10,0);
  lcd.print("s");
-//  afisare timp pauza ventil
+//Afisare timp pauza ventil
  lcd.setCursor(0,1);
  lcd.print("Tpv:");
  lcd.setCursor(4,1);
